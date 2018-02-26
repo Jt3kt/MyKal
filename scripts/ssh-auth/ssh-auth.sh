476b07684c8cb3631203774efa2a753a9780b035
Jtekt <passmossis+gh@gmail.com>
2018-02-26 23:10:59 +0000

    Update to ssh-auth.sh.

--- scripts/ssh-auth/ssh-auth.sh
+++ scripts/ssh-auth/ssh-auth.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #eval $(ssh-agent -s)
-chmod 600 ~/Nextcloud/Projects/Kali/ssh-key/Jtekt
-ssh-add ~/Nextcloud/Projects/Kali/ssh-key/Jtekt
+chmod 600 ~/Nextcloud/Projects/Kali/config/ssh-key/Jtekt
+ssh-add ~/Nextcloud/Projects/Kali/config/ssh-key/Jtekt
 mkdir ~/.ssh
-cp ~/Nextcloud/Projects/Kali/ssh-key/config ~/.ssh/
+cp ~/Nextcloud/Projects/Kali/config/ssh-key/config ~/.ssh/
