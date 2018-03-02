2a615f6edd4706973d6572d92be1b82bbc1b151c
Jtekt <passmossis+gh@gmail.com>
2018-03-02 14:42:03 +0000

    Updated alias.  Reloads bashrc for current shell.  Added basic nmaps.

--- scripts/alias/alias.sh
+++ scripts/alias/alias.sh
@@ -6,15 +6,42 @@ then
 else
 	echo alias lm=\"ls -l \|more\" >> $strBashrc
 fi
+
 if grep -q "ls -lh" "$strBashrc";
 then
 	echo "Alias exists."
 else 
-	echo alias ll=\"ls -lh\" >> /root/.bashrc
+	echo alias ll=\"ls -lh\" >> $strBashrc
 fi
+
 if grep -q "tail -f /var/log/messages" "$strBashrc";
 then
 	echo "Alias exists."
 else
-	echo alias tailm=\"tail -f /var/log/messages\" >> /root/.bashrc
+	echo alias tailm=\"tail -f /var/log/messages\" >> $strBashrc
 fi
+
+if grep -q "sweep" "$strBashrc";
+then
+	echo "Alias exists."
+else
+	echo alias sweep=\"nmap -sP\" >> $strBashrc
+fi
+
+if grep -q "nserv" "$strBashrc";
+then
+	echo "Alias exists."
+else
+	echo alias nserv=\"nmap -p 1-65535 -sV -sS -T3\" >> $strBashrc
+fi
+
+if grep -q "nall" "$strBashrc";
+then
+	echo "Alias exists."
+else
+	echo alias nall=\"nmap -v -sS -A -T3\" >> $strBashrc
+fi
+
+
+#reload .bashrc
+source ~/.bashrc
