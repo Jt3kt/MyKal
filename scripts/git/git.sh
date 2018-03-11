9bbe980ef86481f5d813b4fed4af948b9b45073c
Jtekt <passmossis+gh@gmail.com>
2018-03-11 07:34:29 +0000

    Update e-mail address.

--- scripts/git/git.sh
+++ scripts/git/git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 git config --global user.name "Jtekt"
-git config --global user.email passmossis+gh@gmail.com
+git config --global user.email github.jtekt@gmail.com
 git remote remove origin
 git remote add origin git@github.com:Jtekt/MyKal.git
 git push --set-upstream origin master
