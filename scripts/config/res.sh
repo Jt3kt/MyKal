13b7d075f82efc86c4ca211ca92cedebd4f2f3e0
Jtekt <github.jtekt@gmail.com>
2018-09-04 13:07:36 +0000

    Added 1080p to config

--- scripts/config/res.sh
+++ scripts/config/res.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #Standard : xrandr -s 1280x1024
 #Widescreen : xrandr -s 1680x1050
-echo "Standard or widescreen? (s/w)"
+echo "Standard or widescreen? (s/w/p)"
+echo "s - 1280x1024"
+echo "w - 1680x1050"
+echo "p - 1920x1080"
 read res
 if [ $res = s ];
 then
@@ -9,6 +12,9 @@ then
 elif [ $res = w ]
 then
 	xrandr -s 1680x1050
+elif [ $res = p ]
+then
+	xrandr -s 1920x1080
 fi
 echo "Resolution good? (y/n)"
 read check
